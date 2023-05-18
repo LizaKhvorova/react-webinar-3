3161a3e021c0f50f61b2f9328ee9f52da6436149
Khvorova_Liza <liza.khvorova@gmail.com>
2023-05-18 12:40:11 +0300

    some works from lecture 2

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { createElement } from './utils.js';
-import './styles.css';
+import List from './components/list/index.jsx';
+import Controls from './components/controls/index.jsx';
+import Head from "./components/head/index.jsx";
+import PageLayout from "./components/page-layout/index.jsx";
 
 /**
  * Приложение
@@ -8,37 +10,24 @@ import './styles.css';
  * @returns {React.ReactElement}
  */
 function App({ store }) {
+    const list = store.getState().list;
 
-  const list = store.getState().list;
-
-  return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map((item, index) =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title} {item.highlited ? ` | Выделяли ${item.highlited} раз(a) ` : null}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    const onDeleteItem = (code) => {
+        store.deleteItem(code);
+    }
+    const onSelectItem = (code) => {
+        store.selectItem(code);
+    }
+    const onAddItem = () => {
+        store.addItem();
+    }
+    return (
+       <PageLayout>
+        <Head title={"Приложение на чистом JS"}/>
+        <Controls onAdd={onAddItem}/>
+        <List list={list} onDeleteItem={onDeleteItem} onSelectItem={onSelectItem} />
+       </PageLayout>
+    );
 }
 
 export default App;
